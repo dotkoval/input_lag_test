63442852975bf22e33152d0a2de2201323a3a799
dotkoval <alexander@k0valchuk.com>
2025-11-06 23:01:06 +0100

    Custom TAP and TRD loaders building

--- make.bat
+++ make.bat
@@ -1,8 +1,11 @@
 @echo off
 
+mkdir builds\loader
+
 echo [33m=== REMOVING ARTIFACTS ===[0m
-del /s %~dp0\input_lag_test.tap
-del /s %~dp0\input_lag_test.sna
+del /s/f/q %~dp0\builds\loader\*.*
+del /s/f/q %~dp0\builds\*.*
 
 echo [33m=== BUILDING ZXPROJECT ===[0m
-C:\ZX\Projects\tools\sjasmplus\sjasmplus.exe --msg=war --sld=input_lag_test.sld %~dp0\src\input_lag_test.a80
+sjasmplus.exe --msg=war src/input_lag_test.a80
+sjasmplus.exe --msg=war src/loader.a80
